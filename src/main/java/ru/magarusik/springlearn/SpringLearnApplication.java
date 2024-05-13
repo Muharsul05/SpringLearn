1f8313c5a24ab9383824a3bdd72ae0013618ed93
Magarusik <mrasul13112001@gmail.com>
2024-05-13 13:45:08 +0300

    init commit

---
+++ src/main/java/ru/magarusik/springlearn/SpringLearnApplication.java
@@ -0,0 +1,13 @@
+package ru.magarusik.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+
+}
